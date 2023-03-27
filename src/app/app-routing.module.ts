d80db176b38836bf30797962783be2186adc228e
Antonio <antoniosanguine@gmail.com>
2023-03-27 10:10:02 +0200

    pushingAll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAddComponent } from './pages/users/user-add/user-add.component';
+import { UserHomeComponent } from './pages/users/user-home/user-home.component';
+import { UserListComponent } from './pages/users/user-list/user-list.component';
+import { UserLoginComponent } from './pages/users/user-login/user-login.component';
+import { UserProfilComponent } from './pages/users/user-profil/user-profil.component';
+import { UserAddGalleryComponent } from './pages/users/user-add-gallery/user-add-gallery.component';
+import { CountriesSelectComponent } from './pages/countiries-select/countiries-select.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo: 'home', pathMatch: 'full'},
+  {path : 'home', component : UserHomeComponent},
+  {path : 'userlist', component : UserListComponent},
+  {path : 'inscription', component : UserAddComponent},
+  {path : 'login', component : UserLoginComponent},
+  {path : 'profil', component : UserProfilComponent},
+  {path : 'testselect', component : CountriesSelectComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UserListComponent } from './pages/users/user-list/user-list.component';
+import { UserAddComponent } from './pages/users/user-add/user-add.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ImageServerPipe } from './pipes/image-server.pipe';
+import { UserLoginComponent } from './pages/users/user-login/user-login.component';
+import { UserHomeComponent } from './pages/users/user-home/user-home.component';
+import { AuthInterceptor } from './security/AuthInterceptor.security';
+import { TopBannerComponent } from './banner/top-banner/top-banner.component';
+import { UserProfilComponent } from './pages/users/user-profil/user-profil.component';
+import { ToDatePipe } from './pipes/to-date.pipe';
+import { UserEditComponent } from './pages/users/user-edit/user-edit.component';
+import { DndDirective, UserAddGalleryComponent } from './pages/users/user-add-gallery/user-add-gallery.component';
+import { CountriesSelectComponent } from './pages/countiries-select/countiries-select.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserHomeComponent,
+    UserListComponent,
+    UserAddComponent,
+    UserLoginComponent,
+    ImageServerPipe,
+    TopBannerComponent,
+    UserProfilComponent,
+    ToDatePipe,
+    UserEditComponent,
+    UserAddGalleryComponent,
+    DndDirective,
+    CountriesSelectComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HammerModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
